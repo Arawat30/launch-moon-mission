63e71126daaad687f8a003ab66528b796d2aad40
Anshul <rawat4job@gmail.com>
2025-07-17 15:06:22 +0530

    intial commit to artemis rocket launch

--- Parallax project/moon-mission-clone/src/main.jsx
+++ Parallax project/moon-mission-clone/src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
-import  Chandrayan from "./Chandrayan.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Chandrayan />
+    <App />
   </StrictMode>,
 )
